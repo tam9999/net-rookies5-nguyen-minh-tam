a038195a304dcf115571ccbf6268464cf35103d0
tam9999 <56417750+tam9999@users.noreply.github.com>
2022-07-10 00:59:17 +0700

    Update layout Login and Register Customer Site

--- Assignment/Assignment.CustomerSite/Pages/Components/Auth/Login.cshtml.cs
+++ Assignment/Assignment.CustomerSite/Pages/Components/Auth/Login.cshtml.cs
@@ -1,12 +1,31 @@
+using Assignment.CustomerSite.Models;
+using Assignment.CustomerSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refit;
 
 namespace Assignment.CustomerSite.Pages.Components.Auth
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        
+        private readonly ILogger<LoginModel> _logger;
+        private readonly ICategory _category;
+        public HomeViewModel _home;
+
+        public LoginModel(ILogger<LoginModel> logger)
         {
+            _logger = logger;
+            _category = RestService.For<ICategory>("https://localhost:5445");
+            _home = new HomeViewModel();
         }
+
+        public async Task<IActionResult> OnGetAsync(int productId)
+        {
+            var categories = _category.GetAllCategoryAsync().GetAwaiter().GetResult();
+            _home.Categories = categories;
+            return Page();
+        }
+        
     }
 }
--- Assignment/Assignment.CustomerSite/Pages/Components/Auth/Register.cshtml.cs
+++ Assignment/Assignment.CustomerSite/Pages/Components/Auth/Register.cshtml.cs
@@ -1,12 +1,29 @@
+using Assignment.CustomerSite.Models;
+using Assignment.CustomerSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refit;
 
 namespace Assignment.CustomerSite.Pages.Components.Auth
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly ICategory _category;
+        public HomeViewModel _home;
+
+        public RegisterModel(ILogger<RegisterModel> logger)
         {
+            _logger = logger;
+            _category = RestService.For<ICategory>("https://localhost:5445");
+            _home = new HomeViewModel();
+        }
+
+        public async Task<IActionResult> OnGetAsync(int productId)
+        {
+            var categories = _category.GetAllCategoryAsync().GetAwaiter().GetResult();
+            _home.Categories = categories;
+            return Page();
         }
     }
 }
