54756e2595c87de140724c82f75c8449d48a1e01
tam9999 <56417750+tam9999@users.noreply.github.com>
2022-07-10 01:17:01 +0700

    Update About and Contact  page Customer Site

--- Assignment/Assignment.CustomerSite/Pages/Components/Contact/Contact.cshtml.cs
+++ Assignment/Assignment.CustomerSite/Pages/Components/Contact/Contact.cshtml.cs
@@ -1,12 +1,30 @@
+
+using Assignment.CustomerSite.Models;
+using Assignment.CustomerSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refit;
 
 namespace Assignment.CustomerSite.Pages.Components.Contact
 {
     public class ContactModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<ContactModel> _logger;
+        private readonly ICategory _category;
+        public HomeViewModel _home;
+
+        public ContactModel(ILogger<ContactModel> logger)
         {
+            _logger = logger;
+            _category = RestService.For<ICategory>("https://localhost:5445");
+            _home = new HomeViewModel();
+        }
+
+        public async Task<IActionResult> OnGetAsync(int productId)
+        {
+            var categories = _category.GetAllCategoryAsync().GetAwaiter().GetResult();
+            _home.Categories = categories;
+            return Page();
         }
     }
 }
