3f3715c1b7f64ac552be8dd3577a3f97e40eb54f
tam9999 <56417750+tam9999@users.noreply.github.com>
2022-07-13 11:07:50 +0700

    Delete Wrong Assignment

--- Assignment/Assignment.CustomersSite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment.CustomersSite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
